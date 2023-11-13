4e04ea8843750d3b15d37e02c0ea1d0b04411d43
yuanhan123 <91118992+yuanhan123@users.noreply.github.com>
2023-11-13 19:22:51 +0800

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 set -x
-docker kill db09ffb622d75c7b1d94d823fea44f5d977f78bd391bfc889746d4f62134b8f0
-docker rm db09ffb622d75c7b1d94d823fea44f5d977f78bd391bfc889746d4f62134b8f0
+docker kill my-apache-php-app
+docker rm my-apache-php-app
