44311f8c6520ac1ac5f72279a7bdb49155d2940f
yuanhan123 <91118992+yuanhan123@users.noreply.github.com>
2023-11-13 19:02:30 +0800

    Update kill.sh

--- jenkins/scripts/kill.sh
+++ jenkins/scripts/kill.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
 set -x
-docker kill 3b4ef5e04bbb
-docker rm 3b4ef5e04bbb
+docker kill db09ffb622d75c7b1d94d823fea44f5d977f78bd391bfc889746d4f62134b8f0
+docker rm db09ffb622d75c7b1d94d823fea44f5d977f78bd391bfc889746d4f62134b8f0
