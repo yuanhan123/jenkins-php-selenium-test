fd66e6b11edfcd8d7143191a53ebbc7534de36b1
yuanhan123 <91118992+yuanhan123@users.noreply.github.com>
2023-11-13 18:41:50 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-#docker run -d -p 80:80 --name 3b4ef5e04bbb -v C:/Users/yuanhan/Desktop/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
-docker run -d -p 80:80 --name 3b4ef5e04bbb -v C:/Users/yuanhan/Desktop/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html
+docker run -d -p 80:80 --name 3b4ef5e04bbb -v C:/Users/yuanhan/Desktop/jenkins-php-selenium-test/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 
 sleep 1
 set +x
